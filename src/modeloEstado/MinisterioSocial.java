9bd6da3c8d906086d216617491c593c9bef1af59
vcanor01 <vcanor01@gmail.com>
2021-05-20 19:03:03 +0200

    ministerio hacienda y estado

--- src/modeloEstado/MinisterioSocial.java
+++ src/modeloEstado/MinisterioSocial.java
@@ -1,9 +1,26 @@
+
 package modeloEstado;
+
+import java.util.ArrayList;
+
+import modeloSer.Ser;
 
 /*
  * Se encarga de gestionar menores, ancianos y parados
  * Tambien se encarga del pago de estos sectores
  */
 public class MinisterioSocial {
-
+	
+	private final ArrayList<Ser> menores = new ArrayList<>();
+	private final ArrayList<Ser> ancianos = new ArrayList<>();
+	
+	
+	public ArrayList<Ser> getMenores() {
+		return menores;
+	}
+	public ArrayList<Ser> getAncianos() {
+		return ancianos;
+	}
+	
+	
 }
